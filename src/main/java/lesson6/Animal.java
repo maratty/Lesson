da1fbfc818411303dc7eb58ff1fee0373ba7c835
John Smith <marraty@mail.ru>
2025-03-04 19:01:34 +0300

    04.03

--- src/main/java/lesson6/Animal.java
+++ src/main/java/lesson6/Animal.java
@@ -3,11 +3,12 @@ package lesson6;
 public class Animal {
 
     static int countAnimal = 0;
-    Animal(){
+
+    Animal() {
         countAnimal++;
     }
 
-    public static int getCountAnimal(){
+    public static int getCountAnimal() {
         return countAnimal;
     }
 }
